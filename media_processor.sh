e6ee1827f09361a756824521eec5748479266b08
adeend-co <80993208+adeend-co@users.noreply.github.com>
2026-04-16 22:09:47 +0800

    media_processor.sh を更新

--- media_processor.sh
+++ media_processor.sh
@@ -64,7 +64,7 @@ AGREEMENT_VERSION="1.6"
 SCRIPT_UPDATE_DATE="2026-01-22" # 請根據實際情況修改此日期
 
 # ... 其他設定 ...
-TARGET_DATE="2026-02-23" # <<< 新增：設定您的目標日期
+TARGET_DATE="2026-06-26" # <<< 新增：設定您的目標日期
 # DEFAULT_URL, THREADS, MAX_THREADS, MIN_THREADS 保留
 DEFAULT_URL="https://www.youtube.com/watch?v=siNFnlqtd8M"
 THREADS=4
@@ -608,7 +608,7 @@ display_countdown() {
         seconds=$(( remaining_seconds % 60 ))
 
         # 組合顯示訊息 (使用不同顏色區分)
-        countdown_message="${CYAN}距離「開學」（ ${TARGET_DATE} ）尚餘： ${GREEN}${days} ${WHITE}天 ${GREEN}${hours} ${WHITE}時 ${GREEN}${minutes} ${WHITE}分 ${GREEN}${seconds} ${WHITE}秒${RESET}"
+        countdown_message="${CYAN}距離「暑假」（ ${TARGET_DATE} ）尚餘： ${GREEN}${days} ${WHITE}天 ${GREEN}${hours} ${WHITE}時 ${GREEN}${minutes} ${WHITE}分 ${GREEN}${seconds} ${WHITE}秒${RESET}"
     fi
 
     # 輸出倒數計時訊息
